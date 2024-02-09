99b7f4128ed6f52afda1586bedd26a8a4aa18d6a
Bookkeeper33 <72704006+Bookkeeper33@users.noreply.github.com>
2024-02-09 19:49:49 +0200

    fix: lift up state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import Preview from "./components/preview/Preview";
 import Editable from "./components/sections/Editable";
 
 function App() {
+    const [educationEntries, setEducationEntries] = useState([]);
+    const [personalInfo, setPersonalInfo] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        address: "",
+    });
+
+    const handleEducationEntries = (educationData) => {
+        setEducationEntries([...educationEntries, educationData]);
+    };
+
+    const handleDeleteEducationEntry = (id) => {
+        const educationFiltered = educationEntries.filter(
+            (item) => item.id !== id
+        );
+        setEducationEntries(educationFiltered);
+    };
+
     return (
         <div className="flex min-h-screen flex-col gap-10 p-7 sm:flex-row sm:justify-center ">
-            <Editable />
-            <Preview />
+            <Editable
+                details={personalInfo}
+                onChangeDetails={setPersonalInfo}
+                educationList={educationEntries}
+                onEditEducation={handleEducationEntries}
+                onDeleteEducation={handleDeleteEducationEntry}
+            />
+            <Preview details={personalInfo} educationList={educationEntries} />
         </div>
     );
 }
