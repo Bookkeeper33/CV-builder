92763af1f25ebe092ab91e4fc168f58fa1bb3706
Bookkeeper33 <72704006+Bookkeeper33@users.noreply.github.com>
2024-02-09 19:48:00 +0200

    feature: add personal info section

--- src/components/preview/PersonalInfo.jsx
+++ src/components/preview/PersonalInfo.jsx
@@ -1,27 +1,34 @@
-/* eslint-disable react/prop-types */
-function PersonalInfo({ info }) {
+function PersonalInfo({ details }) {
     return (
         <div className="space-y-5  bg-sky-900 p-5 text-white">
-            <h1 className="text-center text-4xl font-bold">{info.fullName}</h1>
+            <h1 className="text-center text-4xl font-bold">
+                {details.firstName} {details.lastName}
+            </h1>
             <ul className="flex flex-col items-center gap-y-2 *:flex *:items-center *:gap-x-1 sm:flex-row sm:justify-center sm:gap-x-10">
-                <li>
-                    <i className="fa-regular fa-envelope text-lg"></i>
-                    <span className="text-base font-semibold">
-                        {info.email}
-                    </span>
-                </li>
-                <li>
-                    <i className="fa-solid fa-phone text-lg"></i>
-                    <span className="text-base font-semibold">
-                        {info.phone}
-                    </span>
-                </li>
-                <li>
-                    <i className="fa-solid fa-location-dot text-lg"></i>
-                    <span className="text-base font-semibold">
-                        {info.location}
-                    </span>
-                </li>
+                {details.email && (
+                    <li>
+                        <i className="fa-regular fa-envelope text-lg"></i>
+                        <span className="text-base font-semibold">
+                            {details.email}
+                        </span>
+                    </li>
+                )}
+                {details.phone && (
+                    <li>
+                        <i className="fa-solid fa-phone text-lg"></i>
+                        <span className="text-base font-semibold">
+                            {details.phone}
+                        </span>
+                    </li>
+                )}
+                {details.address && (
+                    <li>
+                        <i className="fa-solid fa-location-dot text-lg"></i>
+                        <span className="text-base font-semibold">
+                            {details.address}
+                        </span>
+                    </li>
+                )}
             </ul>
         </div>
     );
